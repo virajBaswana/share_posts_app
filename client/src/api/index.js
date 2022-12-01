2c2045c187a96b2fd9f5f6822916e19bfd091b3c
Viraj Baswana <virajbaswana@gmail.com>
2022-12-01 20:48:29 +0530

    initialised server to push the client side in production

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memoriesbackend-o79e.onrender.com' });
+const API = axios.create({ baseURL: process.env.REACT_BASE_URL || 'http://localhost:5000'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
--- server/index.js
+++ server/index.js
@@ -19,6 +19,19 @@ app.use("/user", userRouter);
 const CONNECTION_URL = 'mongodb+srv://virajBaswana:virajBaswana@cluster0.k0gqiec.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
