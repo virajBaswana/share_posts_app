be716f1c14461d5945cd8054db5c27a56021e450
Viraj Baswana <virajbaswana@gmail.com>
2022-12-01 21:07:55 +0530

    baseURL corrected

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_BASE_URL || 'http://localhost:5000'});
+const API = axios.create({ baseURL: '/'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
