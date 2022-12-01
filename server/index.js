baa06afedb5119492a386dd55ba1a36dd59f4a21
Viraj Baswana <virajbaswana@gmail.com>
2022-12-01 21:49:06 +0530

    __dirname resolved

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
